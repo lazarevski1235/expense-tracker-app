4ec86c05bcd9239f0b20a064c4ae20a3c246645a
Fish12355 <filip.lazarevski1212@gmail.com>
2026-06-28 07:30:48 +0200

    Updated github actions

--- backend/src/test/java/com/expensetracker/ExpenseTrackerApplicationTests.java
+++ backend/src/test/java/com/expensetracker/ExpenseTrackerApplicationTests.java
@@ -2,10 +2,11 @@ package com.expensetracker;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ExpenseTrackerApplicationTests {
-
     @Test
     void contextLoads() {}
 }
